ed9c4425b5349cdd7b0703a2f00c559e60dde6c2
Frank Greco <fgreco@javasig.com>
2025-04-25 14:33:48 -0400

    Remove any references to using any ChatMessage or List of ChatMessages

--- src/main/java/HelloLangChain4j.java
+++ src/main/java/HelloLangChain4j.java
@@ -1,11 +1,7 @@
-import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModelName;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HelloLangChain4j {
     public static void main(String[] argv) {
@@ -16,12 +12,7 @@ public class HelloLangChain4j {
                 .modelName(OpenAiChatModelName.GPT_4_O)
                 .build();
 
-        List<ChatMessage> messages = new ArrayList<>();
-        //UserMessage usrmsg = UserMessage.from("Why should I learn Java.");
         UserMessage usrmsg = UserMessage.from("Why should I learn Java.");
-        //messages.add(usrmsg);
-
-        //var answer = cmodel.chat(messages);
         var answer = cmodel.chat(usrmsg);
 
         System.out.println(answer.aiMessage().text());
