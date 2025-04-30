602c574cb3f0dc5dcaf71cd3cdc6aa38ec6db6a3
Frank Greco <fgreco@javasig.com>
2025-04-29 20:47:48 -0400

    Added usrmsg to ChatMemory after creation.  Switched back to OpenAI from Anthropic.

---
+++ src/main/java/ChatContext.java
@@ -0,0 +1,47 @@
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+
+import java.util.Scanner;
+
+public class ChatContext {
+    public static void main(String[] args) {
+        Scanner userinput;
+        String cmdline;
+        ChatMemory cm = MessageWindowChatMemory.withMaxMessages(100);
+
+        SystemMessage sysmsg = new SystemMessage("""
+                    You are a polite Java consultant with deep expertise in teaching AI and Machine Learning.
+                """);
+        cm.add(sysmsg);
+
+        ChatLanguageModel cmodel = OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(OpenAiChatModelName.GPT_4)
+                .build();
+
+        while (true) {
+            System.out.print("prompt> ");
+
+            userinput = new Scanner(System.in);
+            cmdline = userinput.nextLine();
+
+            if (cmdline.isBlank())       // If nothing, do nothing
+                continue;
+
+            UserMessage usrmsg = UserMessage.from(cmdline);   // create the prompt
+            cm.add(usrmsg);                                   // Add the user prompt to the ChatMemory
+
+            var answer = cmodel.chat(cm.messages());  // send the context as messages and save the response
+            var response = answer.aiMessage().text();
+
+            System.out.println(response);
+
+            cm.add(UserMessage.from(response));     // Add the response from the assistant
+        }
+    }
+}
