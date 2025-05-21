3e787f72589016533ecc3d48aaea9188cc335b1b
fgreco55 <fgreco@javasig.com>
2025-05-20 21:48:47 -0700

    Simple pgm to confirm you have access to an API key

---
+++ src/main/java/Setup.java
@@ -0,0 +1,6 @@
+public class Setup {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(System.getenv("OPENAI_API_KEY"));
+    }
+}
